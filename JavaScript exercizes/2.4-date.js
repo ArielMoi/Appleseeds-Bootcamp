7d240ba5bf6d20036d6fbc9a3be48e7be5a74cdd
Ariel Moise <ariel.m23@gmail.com>
2021-02-07 17:01:34 +0200

    update

--- JavaScript exercizes/2.4-date.js
+++ JavaScript exercizes/2.4-date.js
@@ -1,4 +1,4 @@
 var now = new Date();
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var months = ['january', 'february', 'march', 'april', 'may', 'june', ' july', 'august', 'september', 'october', 'november', 'december']
-console.log('today is ' + days[now.getDay()] + ' the ' + now.getDate() + ' of ' + months[now.getMonth()] + ', ' + now.getFullYear()); 
+var months = ['january', 'february', 'march', 'april', 'may', 'june', ' july', 'august', 'september', 'october', 'november', 'december'];
+console.log(`today is ${days[now.getDay()]} the ${now.getDate()} of ${months[now.getMonth()]}, ${now.getFullYear()}`);
