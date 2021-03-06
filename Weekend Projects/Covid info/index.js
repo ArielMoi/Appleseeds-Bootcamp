637936ddbf8f9a66a42e3a402cdce0bec5752f40
Ariel Moise <ariel.m23@gmail.com>
2021-03-06 16:27:51 +0200

    API weekend project

--- Weekend Projects/Covid info/index.js
+++ Weekend Projects/Covid info/index.js
@@ -34,13 +34,18 @@ async function fetchCovidInfoByCountry() {
 fetchCountriesByregion().then(() => {
     fetchCovidInfoByCountry().then(() => {
 
-        console.log(regionInfo)
-        console.log(coronaInfo)
+        // console.log(regionInfo)
+        // console.log(coronaInfo)
 
         getInfoByregion('Africa');
 
-        
-        console.log(Object.values(currentRegionInfo)[5][1])
+        // console.log(Object.values(currentRegionInfo))
+
+        for (let info of Object.values(currentRegionInfo)){ // ---- creating the chart
+            // console.log(info)
+            info[1][0] && /// info[0] -> country name. info[1] covid data info[1][i] -> covid specific data
+            addData(myChart, info[0], info[1][0])
+        }
 
     })
 })
@@ -50,35 +55,39 @@ function getInfoByregion(region, typeOfInfo = 0) {
     regionInfo[region].forEach(el => {
         el && currentRegionInfo.push([el, coronaInfo[el]])
     })
+
+    currentRegionInfo = Object.values(currentRegionInfo).filter((el) => { /// filter un defined prevents bugs
+        if (el[1]){
+            return true;
+        } else false;
+    })
 }
 
 /// creating the chart - 
 
-// make as function with thw parameters to fill
-// to make background color dinemic create an array with a lot and refrence just the right sum
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
+var ctx = document.getElementById('myChart').getContext('2d'); // element from dom
+var myChart = new Chart(ctx, { /// creating chart with chart js
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Covis-19 World Wide Information',
+            data: [],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                // 'rgba(54, 162, 235, 0.2)',
+                // 'rgba(255, 206, 86, 0.2)', /// commented because becomes irrelevant from too many data
+                // 'rgba(75, 192, 192, 0.2)',
+                // 'rgba(153, 102, 255, 0.2)',
+                // 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                // 'rgba(54, 162, 235, 1)',
+                // 'rgba(255, 206, 86, 1)',
+                // 'rgba(75, 192, 192, 1)',
+                // 'rgba(153, 102, 255, 1)',
+                // 'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
         }]
@@ -94,7 +103,7 @@ var myChart = new Chart(ctx, {
     }
 });
 
-function removeData(chart) {
+function removeData(chart) { /// removes all data from chart
     chart.data.labels = '';
     chart.data.datasets.forEach((dataset) => {
         dataset.data = '';
@@ -102,10 +111,21 @@ function removeData(chart) {
     chart.update();
 }
 
-function addData(chart, label, data) {
+function addData(chart, label, data) { // add data to chart (one in a time so need to be done in loop)
     chart.data.labels.push(label);
     chart.data.datasets.forEach((dataset) => {
         dataset.data.push(data);
     });
     chart.update();
 }
+
+
+/// creating event listners on buttons.
+// each button - 
+// removeData()
+// addData()  -> with current information
+
+// buttons of region will simply update -- currentRegionInfo
+
+
+// add all country names in the region in the HTML
