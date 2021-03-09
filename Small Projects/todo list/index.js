e14ed4a8290448439df78bb8cfe5529203a3b5d1
Ariel Moise <ariel.m23@gmail.com>
2021-03-09 13:14:12 +0200

    to do list

--- Small Projects/todo list/index.js
+++ Small Projects/todo list/index.js
@@ -3,12 +3,15 @@ const toDoListElement = document.querySelector('.todo-list');
 function trashIcon(id) {
     return `<i data-id='${id}' class="far fa-trash-alt"></i>`
 }
-// const trashIcon = '<i class="far fa-trash-alt"></i>';
+
+const ballIcon = '<i class="fas fa-circle"></i>';
+
+const submitNote = document.querySelector('.input-todo button');
 
 const toDoObj = {};
 let id = 0;
 
-function createNote(text, isCompleted = false, priority = 0) {
+function createNote(text, priority = 0, isCompleted = false) {
     let dateOfNote = new Date();
     // toDoObj[++id] = [text, `${dateOfNote.getHours()}:${dateOfNote.getMinutes()},${dateOfNote.getDate()}/${dateOfNote.getMonth() + 1}/${dateOfNote.getFullYear()}`,
     //     priority, isCompleted
@@ -19,18 +22,38 @@ function createNote(text, isCompleted = false, priority = 0) {
     ])
 
     // createNoteInHtml(id) // or in show my notes --- ! 
+    updateNotes();
 }
 
-function createNoteInHtml(id, checked = false) { // publish note -- is called inside create element
+function createNoteInHtml(id, priorityOfNote, checked = false) { // publish note -- is called inside create element
     /// creates al the html needed
     // returns the element// will be used inside create note
+    // debugger;
     let note = document.createElement('div');
     let text = document.createElement('p');
     let checkboxAndButtonDiv = document.createElement('div');
+    let priorityShowcase = document.createElement('i');
     let checkbox = document.createElement('input');
     let button = document.createElement('button');
 
     text.innerText = localStorage.getItem(id).split(',')[0];
+
+    // priority - green ball is 0, yellow is 1, red is 2;
+    // console.log(priorityOfNote);
+    priorityShowcase.innerHTML = ballIcon;
+    switch (priorityOfNote){
+        case '0':
+            // priorityShowcase.classList.add('priority-light')
+            priorityShowcase.style.color = 'green';
+            break;
+        case '1':
+            priorityShowcase.style.color = 'yellow';
+            break;
+        case '2':
+            priorityShowcase.style.color = 'red';
+            break;
+    }
+
     checkbox.setAttribute("type", "checkbox");
     checkbox.setAttribute("data-id", id);
     button.innerHTML = trashIcon(id);
@@ -39,6 +62,7 @@ function createNoteInHtml(id, checked = false) { // publish note -- is called in
         checkbox.checked = true;
     }
 
+    checkboxAndButtonDiv.appendChild(priorityShowcase);
     checkboxAndButtonDiv.appendChild(checkbox);
     checkboxAndButtonDiv.appendChild(button);
 
@@ -50,18 +74,22 @@ function createNoteInHtml(id, checked = false) { // publish note -- is called in
     toDoListElement.appendChild(note);
 }
 
+
 function deleteNote(id) {
     localStorage.removeItem(id)
     updateNotes();
 }
 
-function updateNotes() {
+function updateNotes() { ///// nneed to arrange by priority and then completed
     toDoListElement.innerHTML = '';
     for (let [id, note] of Object.entries(localStorage)) {
-        if (note.split(',')[4] == 'false') createNoteInHtml(id)
+        let priority = note.split(',')[3];
+        // console.log(priority)
+        if (note.split(',')[4] == 'false') createNoteInHtml(id, priority)
     }
     for (let [id, note] of Object.entries(localStorage)) {
-        if (note.split(',')[4] == 'true') createNoteInHtml(id, true)
+        let priority = note.split(',')[3];
+        if (note.split(',')[4] == 'true') createNoteInHtml(id, priority, true)
     }
 }
 
@@ -89,4 +117,20 @@ toDoListElement.addEventListener('click', (e) => {
     } else if (e.target.innerText = 'trash') {
         deleteNote(id)
     }
+})
+
+
+submitNote.addEventListener('click', (e) => {
+    const noteText = document.querySelector('.input-todo textArea');
+    const priority = document.querySelector('select');
+    // console.log()
+    let userPriority;
+    for (let option of priority.options){
+        if (option.selected){
+            userPriority = option.value
+        }
+    }
+    // console.log(userPriority)
+    createNote(noteText.value, userPriority)
+    noteText.value = '';
 })
