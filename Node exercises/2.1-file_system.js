2c6ac53c9f8fd0c4aca9f251f667204b5ad4b1f1
Ariel Moise <ariel.m23@gmail.com>
2021-04-11 14:33:45 +0300

    week 12 - node

--- Node exercises/2.1-file_system.js
+++ Node exercises/2.1-file_system.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'this is a node created text fils');
+fs.writeFileSync('notes.txt', 'this is a node created text fils'); // creates text file
 
-fs.copyFileSync("notes.txt", "copy.txt");
+fs.copyFileSync("notes.txt", "copy.txt"); // creates copy of txt file
+
+
+fs.appendFileSync('notes.txt', 'this was appended to the file')
+
+fs.rename("copy.txt", 'copyRenamed.txt', () => console.log('e')); // rename file
