8ddd5b403cf96701b4bb1b0647d36716b58fadfc
Ariel Moise <ariel.m23@gmail.com>
2021-04-11 16:25:10 +0300

    week 12 - node

--- Node exercises/2.1-file_system.js
+++ Node exercises/2.1-file_system.js
@@ -8,3 +8,15 @@ fs.copyFileSync("notes.txt", "copy.txt"); // creates copy of txt file
 fs.appendFileSync('notes.txt', 'this was appended to the file')
 
 fs.rename("copy.txt", 'copyRenamed.txt', () => console.log('e')); // rename file
+
+function getCurrentFilenames() {
+  /// readdirSync -- returns array with files names
+  console.log("Current filenames:");
+  fs.readdirSync(__dirname).forEach((file) => {
+    console.log(file);
+  });
+}
+getCurrentFilenames();
+
+// read files - 
+console.log(fs.readFileSync("notes.txt", { encoding: "utf8", flag: "r" }));  
---
+++ Node exercises/3.1-import_export.js
@@ -0,0 +1,22 @@
+// const validator = require('validator');
+// const chalk = require('chalk')
+
+// console.log(validator.isEmail('aaa@gmail.com'));
+// console.log(chalk.bgRed('hello'));
+
+/* 
+The major difference between require and import, is that require will automatically scan 
+node_modules to find modules, but import, which comes from ES6, won't.
+also, require will execute the file imported.
+
+Give 2 node.js environment variables that are not available
+when using the import syntax :
+- process
+- global 
+
+
+*/
+
+import {fs} from 'fs';
+
+console.log(fs.readdirSync(__dirname));
