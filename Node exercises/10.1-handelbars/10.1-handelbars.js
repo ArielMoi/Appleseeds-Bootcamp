6a187d306a19749a35a3b5c9c72d4a5246a9ed4e
Ariel Moise <ariel.m23@gmail.com>
2021-04-13 16:03:09 +0300

    week 12 - handelbars

---
+++ Node exercises/10.1-handelbars/10.1-handelbars.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const express = require("express");
+const fs = require('fs')
+// const index = fs.readFileSync("./public/index.html");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const publicDirectoryPath = path.join(__dirname, "../public")
+
+app.set('view engine', 'hbs')
+app.use(express.static(publicDirectoryPath));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: 'Index'
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: 'about'
+  });
+});
+
+
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
